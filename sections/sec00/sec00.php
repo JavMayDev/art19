d59273d9f10c1b1b0f9630701b2d400ac71f3e4f
maydev <javimayorque@gmail.com>
2021-03-22 16:07:11 -0600

    remove info from section 00

--- sections/sec00/sec00.php
+++ sections/sec00/sec00.php
@@ -16,29 +16,5 @@
 	</div>
     </div>
 
-    <div class="info" getin="toggleChart">
-	<div style="display: flex; flex-direction: row; margin-top: 50px;">
-	    <!--lorem-->
-	    <p style="margin: 30px">Lacus. Vestibulum tempor lectus at est. Pellentesque habitant morbi
-	    tristique senectus et netus et malesuada fames ac turpis egestas. Sed vitae
-	    eros. Nulla pulvinar turpis eget nunc. Sed bibendum pellentesque nunc. Integer
-	    tristique, lorem ac faucibus tempor, lorem dolor mollis turpis, a consectetuer
-	    nunc justo ac nisl.</p>
-
-	    <p style="margin: 30px">Nam vitae purus. Cum sociis natoque penatibus et magnis dis parturient
-	    montes, nascetur ridiculus mus. Praesent semper magna. In eu justo. Nunc vitae
-	    risus nec sem scelerisque consequat. In hac habitasse platea dictumst. Nam
-	    posuere ultricies turpis. Pellentesque a pede. Duis sed tortor. Phasellus
-	    egestas porta lectus. Aliquam dignissim consequat diam. Pellentesque pede.</p>
-	</div>
-
-	<div id="chart00-wrapper">
-	    <svg id="chart00" style="width: 100%; height: 500px"></svg>
-	</div>
-    </div>
-    <div class="info">some</div>
-    <div class="info">info</div>
-    <div class="info">page</div>
-
     <script src="sections/sec00/init00cover.js"></script>
 </div>
